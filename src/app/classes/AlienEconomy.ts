09b92cb88c80bc0ee2cda823241a9f4425f7ca6d
Al Morton <morton_al@hotmail.com>
2019-06-01 17:58:01 +0900

    Turn class created

---
+++ src/app/classes/AlienEconomy.ts
@@ -0,0 +1,8 @@
+export class AlienEconomy {
+    public colonies: number;
+    public fleet: number;
+    public bank: number;
+    public tech: number;
+    public defenseMax: number;
+    public fleetLaunch: number;
+}
